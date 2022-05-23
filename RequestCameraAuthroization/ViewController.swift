47d5b4472dcb201d3048dacc4895c97e22792192
Hashaam Siddiq <hashaam83@gmail.com>
2022-05-23 20:37:54 +0400

    Initial commit

--- RequestCameraAuthroization/ViewController.swift
+++ RequestCameraAuthroization/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let authorizationStatus = getCameraAuthorization()
+        switch authorizationStatus {
+        case .notDetermined:
+            print("Camera permission has not yet granted. We can request camera permission from the user")
+            
+            requestCameraAuthorization { granted in
+                let grantedStatus = granted ? "granted" : "not granted"
+                print("Camera permission was \(grantedStatus)")
+            }
+            
+        case .restricted:
+            print("User is not allowed to access media capture device.")
+            
+        case .denied:
+            print("User has denied accessing camera. We need to request user to allow the camera permission from settings.")
+            
+        case .authorized:
+            print("Camera permission was granted")
+            
+        @unknown default:
+            print("Camera authorization status is unknown.")
+            
+        }
+        
     }
-
-
+    
+    func getCameraAuthorization() -> AVAuthorizationStatus {
+        let authorizationStatus = AVCaptureDevice.authorizationStatus(for: .video)
+        return authorizationStatus
+    }
+    
+    func requestCameraAuthorization(completionHandler: @escaping (Bool) -> Void) {
+        AVCaptureDevice.requestAccess(for: .video) { granted in
+            completionHandler(granted)
+        }
+    }
+    
 }
 
